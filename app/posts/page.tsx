fcf8d4a28936d0f64ced939856df8d38bdc3c418
Muhammed Shakir C K <shakirck333@gmail.com>
2023-03-11 11:55:15 +0530

    fix large image in posts

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,19 +1,17 @@
-import React from 'react';
-import SocialIcons from '../components/socialIcons';
 import './posts.css';
- 
-  
+
 export default async function page() {
   const res = await fetch(
     'https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@shakirck'
-    ,{
-      next:{
+    , {
+      next: {
         revalidate: 120
       }
     }
   );
   const data = await res.json();
   const posts = data.items;
+  let image = "";
   const getFirst250Words = (text: string) => {
     const words = text.split(' ');
     if (words.length > 50) {
@@ -22,15 +20,28 @@ export default async function page() {
       return text;
     }
   };
+  const strippedHTML = (post: string) => {
 
+    const text = getFirst250Words(post);
+
+    const strippedHTML = text.replace(/<figure>.*<\/figure>/g, '');
+    // get the image from the figure tag
+    const image = post.match(/<figure>.*<\/figure>/g);
+    return strippedHTML || text;
+
+  }
   return (
     <div className='blog-container ' >
       {posts.map((post: any) => (
         <div className='blog-post' key={post.guid}>
           <p>{new Date(post.pubDate).toLocaleDateString()}</p>
           <h1>{post.title}</h1>
+          <div className='header-image'
+          >
+            <img src={post.thumbnail} width="400px" alt="thumb" />
+          </div>
           <div
-            dangerouslySetInnerHTML={{ __html: getFirst250Words(post.description) }}
+            dangerouslySetInnerHTML={{ __html: strippedHTML(post.description) }}
           />
           <a href={post.link}>Read More</a>
         </div>
